2ba065300d2172d9e934d654c04836105e81bd3c
Biternes <lttuan96@gmail.com>
2024-08-27 20:46:55 +0700

    Discard wrapper component

--- Chathub.UI/src/App.tsx
+++ Chathub.UI/src/App.tsx
@@ -1,23 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAppSelector } from './redux/hook';
+import { selectUserInfo, LoginState } from './redux/reducers/userReducer';
 import HomePage from './pages/Home';
 import LoginPage from './pages/Login';
 import SignupPage from './pages/Signup';
-import CheckLoginInfoWrapper from './pages/Components/CheckInfoWrapper';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
 
 function App() {
-  const CheckLoginInfoPage = () => CheckLoginInfoWrapper()(HomePage)
+  const { loginState } = useAppSelector(selectUserInfo);
+  const CheckUserInfoMissing = (RenderPage: React.ComponentType) => loginState === LoginState.LoggedIn ? <RenderPage /> : <Navigate to="/login" />
+  const CheckUserInfoExists = (RenderPage: React.ComponentType) => loginState !== LoginState.LoggedIn ? <RenderPage /> : <Navigate to="/" />
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<CheckLoginInfoPage/>} />
-          <Route path="/login" element={<LoginPage />}/>
-          <Route path="/signup" element={<SignupPage/>}/>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={CheckUserInfoMissing(HomePage)} />
+        <Route path="/login" element={CheckUserInfoExists(LoginPage)} />
+        <Route path="/signup" element={CheckUserInfoExists(SignupPage)} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
